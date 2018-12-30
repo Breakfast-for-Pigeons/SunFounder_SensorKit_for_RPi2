df4247b156fa149bb4f4df35e4345a6854412d04
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-12-30 14:30:04 -0600

    Update 10_passive_buzzer.py

--- Python/10_passive_buzzer.py
+++ Python/10_passive_buzzer.py
@@ -1,77 +1,95 @@
 #!/usr/bin/env python
-#---------------------------------------------------
-#
-#	This is a program for Passive Buzzer Module
-#		It will play simple songs.
-#	You could try to make songs by youselves!
-# 
-#		Passive buzzer 			   Pi 
-#			VCC ----------------- 3.3V
-#			GND ------------------ GND
-#			SIG ---------------- Pin 11
-#
-#---------------------------------------------------
+"""
+-----------------------------------------------------
 
-import RPi.GPIO as GPIO
-import time
+  This is a program for the Sunfounder passive buzzer module.
+  It will play simple songs.
+  You could try to make songs by youselves!
 
-Buzzer = 11
+        Passive buzzer             Pi
+            VCC ----------------- 3.3V
+            GND ------------------ GND
+            SIG ---------------- Pin 11
 
-CL = [0, 131, 147, 165, 175, 196, 211, 248]		# Frequency of Low C notes
+-----------------------------------------------------
+"""
+from time import sleep
+from gpiozero import PWMOutputDevice
 
-CM = [0, 262, 294, 330, 350, 393, 441, 495]		# Frequency of Middle C notes
+buzzer = PWMOutputDevice(pin=17, active_high=True, initial_value=0.5,
+                         frequency=440)
 
-CH = [0, 525, 589, 661, 700, 786, 882, 990]		# Frequency of High C notes
+CL = [0, 131, 147, 165, 175, 196, 211, 248]  # Frequency of Low C notes
 
-song_1 = [	CM[3], CM[5], CM[6], CM[3], CM[2], CM[3], CM[5], CM[6], # Notes of song1
-			CH[1], CM[6], CM[5], CM[1], CM[3], CM[2], CM[2], CM[3], 
-			CM[5], CM[2], CM[3], CM[3], CL[6], CL[6], CL[6], CM[1],
-			CM[2], CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]	]
+CM = [0, 262, 294, 330, 350, 393, 441, 495]  # Frequency of Middle C notes
 
-beat_1 = [	1, 1, 3, 1, 1, 3, 1, 1, 			# Beats of song 1, 1 means 1/8 beats
-			1, 1, 1, 1, 1, 1, 3, 1, 
-			1, 3, 1, 1, 1, 1, 1, 1, 
-			1, 2, 1, 1, 1, 1, 1, 1, 
-			1, 1, 3	]
+CH = [0, 525, 589, 661, 700, 786, 882, 990]  # Frequency of High C notes
 
-song_2 = [	CM[1], CM[1], CM[1], CL[5], CM[3], CM[3], CM[3], CM[1], # Notes of song2
-			CM[1], CM[3], CM[5], CM[5], CM[4], CM[3], CM[2], CM[2], 
-			CM[3], CM[4], CM[4], CM[3], CM[2], CM[3], CM[1], CM[1], 
-			CM[3], CM[2], CL[5], CL[7], CM[2], CM[1]	]
+# Notes of song1
+song_1 = [CM[3], CM[5], CM[6], CM[3], CM[2], CM[3], CM[5], CM[6],
+          CH[1], CM[6], CM[5], CM[1], CM[3], CM[2], CM[2], CM[3],
+          CM[5], CM[2], CM[3], CM[3], CL[6], CL[6], CL[6], CM[1],
+          CM[2], CM[3], CM[2], CL[7], CL[6], CM[1], CL[5]]
 
-beat_2 = [	1, 1, 2, 2, 1, 1, 2, 2, 			# Beats of song 2, 1 means 1/8 beats
-			1, 1, 2, 2, 1, 1, 3, 1, 
-			1, 2, 2, 1, 1, 2, 2, 1, 
-			1, 2, 2, 1, 1, 3 ]
+# Beats of song 1, 1 means 1/8 beats
+beat_1 = [1, 1, 3, 1, 1, 3, 1, 1,
+          1, 1, 1, 1, 1, 1, 3, 1,
+          1, 3, 1, 1, 1, 1, 1, 1,
+          1, 2, 1, 1, 1, 1, 1, 1,
+          1, 1, 3]
 
-def setup():
-	GPIO.setmode(GPIO.BOARD)		# Numbers GPIOs by physical location
-	GPIO.setup(Buzzer, GPIO.OUT)	# Set pins' mode is output
-	global Buzz						# Assign a global variable to replace GPIO.PWM 
-	Buzz = GPIO.PWM(Buzzer, 440)	# 440 is initial frequency.
-	Buzz.start(50)					# Start Buzzer pin with 50% duty ration
+# Notes of song2
+song_2 = [CM[1], CM[1], CM[1], CL[5], CM[3], CM[3], CM[3], CM[1],
+          CM[1], CM[3], CM[5], CM[5], CM[4], CM[3], CM[2], CM[2],
+          CM[3], CM[4], CM[4], CM[3], CM[2], CM[3], CM[1], CM[1],
+          CM[3], CM[2], CL[5], CL[7], CM[2], CM[1]]
 
-def loop():
-	while True:
-		print '\n    Playing song 1...'
-		for i in range(1, len(song_1)):		# Play song 1
-			Buzz.ChangeFrequency(song_1[i])	# Change the frequency along the song note
-			time.sleep(beat_1[i] * 0.5)		# delay a note for beat * 0.5s
-		time.sleep(1)						# Wait a second for next song.
+# Beats of song 2, 1 means 1/8 beats
+beat_2 = [1, 1, 2, 2, 1, 1, 2, 2,
+          1, 1, 2, 2, 1, 1, 3, 1,
+          1, 2, 2, 1, 1, 2, 2, 1,
+          1, 2, 2, 1, 1, 3]
 
-		print '\n\n    Playing song 2...'
-		for i in range(1, len(song_2)):     # Play song 1
-			Buzz.ChangeFrequency(song_2[i]) # Change the frequency along the song note
-			time.sleep(beat_2[i] * 0.5)     # delay a note for beat * 0.5s
 
-def destory():
-	Buzz.stop()					# Stop the buzzer
-	GPIO.output(Buzzer, 1)		# Set Buzzer pin to High
-	GPIO.cleanup()				# Release resource
+def passive_buzzer():
+    """
+    Plays songs on the active buzzer
+    """
+    while True:
+        print("\nPlaying song 1...")
+        # Play song 1
+        for i in range(1, len(song_1)):
+            # Change the frequency along the song note
+            buzzer.frequency = song_1[i]
+            # delay a note for beat * 0.5s
+            sleep(beat_1[i] * 0.5)
+        # Wait a second for next song.
+        sleep(1)
 
-if __name__ == '__main__':		# Program start from here
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:  	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destory()
+        print("\nPlaying song 2...")
+        # Play song 2
+        for i in range(1, len(song_2)):
+            # Change the frequency along the song note
+            buzzer.frequency = song_2[i]
+            # delay a note for beat * 0.5s
+            sleep(beat_2[i] * 0.5)
+        # Wait a second for next song.
+        sleep(1)
+
+
+def stop():
+    """
+    Releases resources and exits.
+    """
+    print("\nStopping program.")
+    buzzer.off()
+    buzzer.close()
+    exit()
+
+
+if __name__ == '__main__':      # Program start from here
+    try:
+        passive_buzzer()
+    # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+    except KeyboardInterrupt:
+        stop()
