7eea5aa7ff83268c5701721f81afaf9d3f503198
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2019-01-01 20:08:33 -0600

    Update 12_photo_interrupter.py

--- Python/12_photo_interrupter.py
+++ Python/12_photo_interrupter.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python
-import RPi.GPIO as GPIO
+"""
+Controls the SunFounder photo interupter module.
 
-PIPin  = 11
-Gpin   = 12
-Rpin   = 13
+When the photo interupter switch is activated, it changes the color of
+an LED.
 
-def setup():
-	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-	GPIO.setup(Gpin, GPIO.OUT)     # Set Green Led Pin mode to output
-	GPIO.setup(Rpin, GPIO.OUT)     # Set Red Led Pin mode to output
-	GPIO.setup(PIPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
-	GPIO.add_event_detect(PIPin, GPIO.BOTH, callback=detect, bouncetime=200)
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+from time import sleep
+from gpiozero import PWMLED, Button
 
-def Led(x):
-	if x == 0:
-		GPIO.output(Rpin, 1)
-		GPIO.output(Gpin, 0)
-	if x == 1:
-		GPIO.output(Rpin, 0)
-		GPIO.output(Gpin, 1)
+photo_interupter = Button(pin=17, pull_up=False, bounce_time=200,
+                          hold_time=1, hold_repeat=False)
+red = PWMLED(pin=18, active_high=True, initial_value=1, frequency=100)
+green = PWMLED(pin=27, active_high=True, initial_value=0, frequency=100)
 
-def Print(x):
-	if x == 1:
-		print '    *************************'
-		print '    *   Light was blocked   *'
-		print '    *************************'
 
-def detect(chn):
-	Led(GPIO.input(PIPin))
-	Print(GPIO.input(PIPin))
+def print_message():
+    """
+    Prints a message to the screen letting the user know the photo
+    interupter was activated.
+    """
+    print('*************************')
+    print('*   Light was blocked   *')
+    print('*************************')
+    sleep(2)
 
-def loop():
-	while True:
-		pass
 
-def destroy():
-	GPIO.output(Gpin, GPIO.HIGH)       # Green led off
-	GPIO.output(Rpin, GPIO.HIGH)       # Red led off
-	GPIO.cleanup()                     # Release resource
+def stop():
+    """
+    Releases resources and exits.
+    """
+    print("\nStopping program.")
+    photo_interupter.close()
+    red.close()
+    green.close()
+    exit()
 
-if __name__ == '__main__':     # Program start from here
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		destroy()
 
+if __name__ == '__main__':
+    print("Press Crtl-C to stop the program.")
+    try:
+        while True:
+            if photo_interupter.is_pressed:
+                red.off()
+                green.on()
+                print_message()
+            else:
+                red.on()
+    except KeyboardInterrupt:
+        stop()
